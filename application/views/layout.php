2e714bde99fbfeb373288c8f2a211f07977d3ae9
Leslie Michael Orchard <l.m.orchard@pobox.com>
2009-02-23 20:19:17 -0500

    No need for haloscan include when discus used

--- application/views/layout.php
+++ application/views/layout.php
@@ -2,7 +2,6 @@
     <head>
         <meta http-equiv="content-type" content="text/html;charset=utf-8">
         <link rel="stylesheet" type="text/css" href="<?= url::base() ?>css/bucket.css" />
-        <script type="text/javascript" src="http://www.haloscan.com/load/lmorchard"> </script>
         <script type="text/javascript" src="<?= url::base() ?>js/mootools-1.2.1-core-yc.js"></script>
         <script type="text/javascript" src="<?= url::base() ?>js/mootools-1.2-more.js"></script>
         <script type="text/javascript" src="<?= url::base() ?>js/bucket.js"></script>
